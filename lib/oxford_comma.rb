df021de7608cd03c493584734b22f42357b9834a
Wen Xiao <wennstyle@gmail.com>
2022-04-22 17:14:21 -0400

    commit tree b755e14650895a495839093a454f0eb428f0e4ad

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
 def oxford_comma(array)
-
+    
+if array.length == 1
+    array.join 
+    
+else
+    last_element = array.pop()
+    if array.length == 1
+        return "#{array.join(", ")} and #{last_element}"
+    else return "#{array.join(", ")}, and #{last_element}"
+    end
 end
+    
+end
+
+# def oxford_comma(array)
+#     return array.join(" and ") if array.size < 3
+  
+#     # insert 'and ' in front of the last string in the array
+#     array[-1] = "and #{array[-1]}"
+  
+#     array.join(", ")  
+#   end
